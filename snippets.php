891eff5cef3ab14566d1ca5ad6e44120e856ee21
Adam Champagne <achampagne@sussmanconsultants.com>
2018-03-23 08:08:11 -0400

    added acf snippet

--- snippets.php
+++ snippets.php
@@ -3,8 +3,16 @@
 // Execute code based on post ID
 <?php global $post; 
 
-  if( $post->ID == 346) { ?>
+   if( $post->ID == 346) { ?>
 
       <!-- do your stuff here -->
 
-<?php } ?>
+   <?php } ?>
+
+// Echo the featured image URL
+<?php echo get_the_post_thumbnail_url() ?>
+
+// Advanced custom fields remove p tags from being inserted
+<?php
+$variable 			= get_field('variable', false, false);
+?>
