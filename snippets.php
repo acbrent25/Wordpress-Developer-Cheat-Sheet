113debc14b323b21e72f2ffd5eafc5fb3ec5ddaa
Adam Champagne <adamchampagne@gmail.com>
2018-03-27 10:26:33 -0400

    Update snippets.php

--- snippets.php
+++ snippets.php
@@ -17,3 +17,38 @@ $variable 			= get_field('variable', false, false);
 
 // Echo the current year only
 <?php echo date('Y'); ?>
+
+// Hide editor on specific pages.
+
+add_action( 'admin_init', 'hide_editor' );
+function hide_editor() {
+	  // Get the Post ID.
+	  $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+	  
+	  if( !isset( $post_id ) ) return;
+	  
+
+	  $pgname = get_the_title($post_id);
+	  
+	  if($pgname == 'Home' || 'Strategic Support' || 'Customer Service' ){ 
+	    remove_post_type_support('page', 'editor');
+  	  }
+  
+}
+
+// Change Title on Post Types
+function wpb_change_title_text( $title ){
+     $screen = get_current_screen();
+  
+     if  ( 'testimonial' == $screen->post_type ) {
+          $title = 'Customer Name';
+     }  elseif ( 'team_member' == $screen->post_type ) {
+          $title = 'Team Member Name';
+     } 	  elseif ( 'support_features' == $screen->post_type ) {
+          $title = 'Support Feature Title';
+     }     
+     return $title;           
+}
+  
+add_filter( 'enter_title_here', 'wpb_change_title_text' );
+
