75128e9471334fb779bede6dff790a0a0924480e
Adam Champagne <adamchampagne@gmail.com>
2018-03-27 08:25:28 -0400

    Update snippets.php

--- snippets.php
+++ snippets.php
@@ -1,13 +1,11 @@
 
 
 // Execute code based on post ID
-<?php global $post; 
+<?php  is_single('17') { ?>
 
-   if( $post->ID == 346) { ?>
+   <!-- do your stuff here -->
 
-      <!-- do your stuff here -->
-
-   <?php } ?>
+<?php } ?>
 
 // Echo the featured image URL
 <?php echo get_the_post_thumbnail_url() ?>
@@ -16,3 +14,6 @@
 <?php
 $variable 			= get_field('variable', false, false);
 ?>
+
+// Echo the current year only
+<?php echo date('Y'); ?>
